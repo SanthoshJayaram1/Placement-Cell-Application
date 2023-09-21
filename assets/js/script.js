5d941b0f5537d59a248289a4c95375ee85e477ed
SanthoshJayaram1 <santhoshjayaram193@gmail.com>
2023-09-21 11:57:39 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,24 @@
-
+// get two input references for DOM  manupulation
 const companyID=document.getElementById("cName");
 const date=document.getElementById("iDate");
 
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Select all buttons with the class "specific-class"
+    // Select all buttons with the class "btn"
     const addBtn = document.querySelectorAll('.btn');
-    console.log(addBtn);
     // Convert the NodeList to an array
     const buttonArray = Array.from(addBtn);
-
-    // Now, you can iterate through 'buttonArray' and perform operations as needed
+    // Now, iterate through 'buttonArray' and add the event click listener
     buttonArray.forEach((btn) => btn.addEventListener("click", automaticFill));
 });
 
 // to automatically fill inside input field
 function automaticFill(){
+    // it fetch data present in parent html
     const name=this.parentElement.children[0].children[0].children[0].innerHTML;
     const date=this.parentElement.children[1].children[1].children[0].innerHTML;
     const companyID=document.getElementById("cName");
     const iDate=document.getElementById("iDate");
+    // and update inside input upon clcking
     companyID.value=name;
     iDate.value=date;
 }
